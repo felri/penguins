93374cf1fbbd8d841a976c4753e17c12f08cd4e0
felri <felipercmelo@gmail.com>
2023-03-07 19:25:04 -0300

    remove old glsl files

--- src/components/background/index.jsx
+++ src/components/background/index.jsx
@@ -1,53 +1,4 @@
-import { Canvas, useFrame } from "@react-three/fiber";
-import { Suspense, useEffect, useRef, useState } from "react";
-import vertexShader from "./main.vert";
-import fragmentShader from "./main.frag";
-import { Transition } from "react-transition-group";
-import * as THREE from "three";
 import "./styles.css";
-
-// function Backgroung() {
-//   const meshRef = useRef();
-//   const shaderMaterialRef = useRef();
-//   const [mouse, setMouse] = useState([0, 0]);
-//   const resolution = [window.innerWidth, window.innerHeight];
-
-//   useEffect(() => {
-//     const handleResize = () => {
-//       resolution[0] = window.innerWidth;
-//       resolution[1] = window.innerHeight;
-//       meshRef.current.material.uniforms.u_resolution.value = resolution;
-//       meshRef.current.material.uniforms.u_resolution.needsUpdate = true;
-//     };
-
-//     window.addEventListener("resize", handleResize);
-//     return () => {
-//       window.removeEventListener("resize", handleResize);
-//     };
-//   }, []);
-
-//   useFrame(({ clock }) => {
-//     if (!meshRef.current) return;
-//     meshRef.current.material.uniforms.u_time.value = clock.elapsedTime;
-//   });
-
-//   return (
-//     <mesh ref={meshRef}>
-//       <planeGeometry args={resolution} />
-//       <shaderMaterial
-//         ref={shaderMaterialRef}
-//         vertexShader={vertexShader}
-//         fragmentShader={fragmentShader}
-//         uniforms={{
-//           u_zoom: { value: 2.0 },
-//           u_time: { value: 0 },
-//           u_mouse: { value: mouse },
-//           u_resolution: { value: resolution },
-//         }}
-//       />
-//     </mesh>
-//   );
-// }
 
 function Mountain() {
   return (
@@ -70,6 +21,7 @@ function Cloud({ size }) {
     </div>
   );
 }
+
 function CanvasWrapper({ showBackground }) {
   return (
     <div className="background-container">
